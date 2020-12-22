9c3809d7080edfbfb57e9b60c22e6d99bf63c4aa
phuocleoceo <ht10082001@gmail.com>
2020-12-22 15:51:12 +0700

    BackUp

--- Coffee-Management/FormCombo.Designer.cs
+++ Coffee-Management/FormCombo.Designer.cs
@@ -28,47 +28,139 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnCombo = new System.Windows.Forms.Button();
+            this.txtMoney = new System.Windows.Forms.TextBox();
+            this.txtDiscount = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbType = new System.Windows.Forms.ComboBox();
+            this.txtType = new System.Windows.Forms.TextBox();
+            this.lsvResult = new System.Windows.Forms.ListView();
             this.btnAddType = new System.Windows.Forms.Button();
-            this.lvType = new System.Windows.Forms.ListView();
             this.SuspendLayout();
+            // 
+            // btnCombo
+            // 
+            this.btnCombo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCombo.Font = new System.Drawing.Font("Cambria", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCombo.Location = new System.Drawing.Point(364, 238);
+            this.btnCombo.Name = "btnCombo";
+            this.btnCombo.Size = new System.Drawing.Size(126, 72);
+            this.btnCombo.TabIndex = 1;
+            this.btnCombo.Text = "Combo";
+            this.btnCombo.UseVisualStyleBackColor = true;
+            this.btnCombo.Click += new System.EventHandler(this.btnCombo_Click);
+            // 
+            // txtMoney
+            // 
+            this.txtMoney.Font = new System.Drawing.Font("Cambria", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMoney.Location = new System.Drawing.Point(24, 59);
+            this.txtMoney.Name = "txtMoney";
+            this.txtMoney.Size = new System.Drawing.Size(124, 28);
+            this.txtMoney.TabIndex = 2;
+            // 
+            // txtDiscount
+            // 
+            this.txtDiscount.Font = new System.Drawing.Font("Cambria", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDiscount.Location = new System.Drawing.Point(175, 59);
+            this.txtDiscount.Name = "txtDiscount";
+            this.txtDiscount.Size = new System.Drawing.Size(117, 28);
+            this.txtDiscount.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(32, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Money (VND)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cambria", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(176, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Discount (%)";
+            // 
+            // cbType
+            // 
+            this.cbType.Font = new System.Drawing.Font("Cambria", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbType.FormattingEnabled = true;
+            this.cbType.Location = new System.Drawing.Point(46, 106);
+            this.cbType.Name = "cbType";
+            this.cbType.Size = new System.Drawing.Size(221, 30);
+            this.cbType.TabIndex = 4;
+            // 
+            // txtType
+            // 
+            this.txtType.Font = new System.Drawing.Font("Cambria", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtType.Location = new System.Drawing.Point(24, 222);
+            this.txtType.Multiline = true;
+            this.txtType.Name = "txtType";
+            this.txtType.ReadOnly = true;
+            this.txtType.Size = new System.Drawing.Size(268, 332);
+            this.txtType.TabIndex = 5;
+            // 
+            // lsvResult
+            // 
+            this.lsvResult.Font = new System.Drawing.Font("Cambria", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lsvResult.HideSelection = false;
+            this.lsvResult.Location = new System.Drawing.Point(574, 59);
+            this.lsvResult.Name = "lsvResult";
+            this.lsvResult.Size = new System.Drawing.Size(292, 445);
+            this.lsvResult.TabIndex = 6;
+            this.lsvResult.UseCompatibleStateImageBehavior = false;
+            this.lsvResult.View = System.Windows.Forms.View.Tile;
             // 
             // btnAddType
             // 
             this.btnAddType.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnAddType.Font = new System.Drawing.Font("Cambria", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAddType.Location = new System.Drawing.Point(80, 156);
+            this.btnAddType.Location = new System.Drawing.Point(80, 154);
             this.btnAddType.Name = "btnAddType";
             this.btnAddType.Size = new System.Drawing.Size(144, 45);
             this.btnAddType.TabIndex = 1;
             this.btnAddType.Text = "Add";
             this.btnAddType.UseVisualStyleBackColor = true;
-            // 
-            // lvType
-            // 
-            this.lvType.HideSelection = false;
-            this.lvType.Location = new System.Drawing.Point(34, 221);
-            this.lvType.Name = "lvType";
-            this.lvType.Size = new System.Drawing.Size(230, 333);
-            this.lvType.TabIndex = 2;
-            this.lvType.UseCompatibleStateImageBehavior = false;
+            this.btnAddType.Click += new System.EventHandler(this.btnAddType_Click_1);
             // 
             // FormCombo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(939, 578);
-            this.Controls.Add(this.lvType);
+            this.Controls.Add(this.lsvResult);
+            this.Controls.Add(this.txtType);
+            this.Controls.Add(this.cbType);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDiscount);
+            this.Controls.Add(this.txtMoney);
             this.Controls.Add(this.btnAddType);
+            this.Controls.Add(this.btnCombo);
             this.Font = new System.Drawing.Font("Cambria", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "FormCombo";
             this.Text = "C o m b o";
             this.Load += new System.EventHandler(this.FormReporting_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.Button btnCombo;
+        private System.Windows.Forms.TextBox txtMoney;
+        private System.Windows.Forms.TextBox txtDiscount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbType;
+        private System.Windows.Forms.TextBox txtType;
+        private System.Windows.Forms.ListView lsvResult;
         private System.Windows.Forms.Button btnAddType;
-        private System.Windows.Forms.ListView lvType;
     }
 }
--- Coffee-Management/FormCombo.cs
+++ Coffee-Management/FormCombo.cs
@@ -30,8 +30,143 @@ namespace GUILayer
         private void FormReporting_Load(object sender, EventArgs e)
         {
             LoadTheme();
+            LoadCBType();
+            BUS_Drink.Instance.GetList(listDrink);
+            QuickSort(listDrink, 0, listDrink.Count - 1);
         }
-        /*---------------------------------------------------------------------*/
+        /*-------------------------------------------------------------------------------------*/
+        private void QuickSort(ManageList<Drink> list, int left, int right)
+        {
+            if (left <= right)
+            {
+                int i = left, j = right;
+                Drink x = list[(left + right) / 2];
+                while (i <= j)
+                {
+                    while (list[i].Price > x.Price) i++;
+                    while (list[j].Price < x.Price) j--;
+                    if (i <= j)
+                    {
+                        Drink temp = list[i];
+                        list[i] = list[j];
+                        list[j] = temp;
+                        i++;
+                        j--;
+                    }
+                }
+                if (left < j) QuickSort(list, left, j);
+                if (i < right) QuickSort(list, i, right);
+            }
+        }
+        /*-------------------------------------------------------------------------------------*/
+
         private ManageList<DrinkType> listType = new ManageList<DrinkType>();
+        private ManageList<string> listChooseType = new ManageList<string>();
+        //Load List DrinkType vào Combobox , chọn từng cái rồi bấm Add để thêm vào listChooseType
+        private void LoadCBType()
+        {
+            BUS_DrinkType.Instance.GetList(listType);
+            for (int i = 0; i < listType.Count; i++)
+            {
+                cbType.Items.Add(listType[i].Name);
+            }
+        }
+        private int dem = 1;
+
+        private void btnAddType_Click_1(object sender, EventArgs e)
+        {
+            if (cbType.Text != String.Empty)
+            {
+                listChooseType.Add(cbType.Text);
+                txtType.Text += (dem++).ToString() + ". " + cbType.Text + "\r\n";
+                cbType.Items.Remove(cbType.Text);
+            }
+        }
+        /*-------------------------------------------------------------------------------------*/
+
+        //list là List lưu trữ các List Drink tương ứng với từng DrinkType đã được chọn ở trên
+        private ManageList<Drink> listDrink = new ManageList<Drink>();
+        private ManageList<ManageList<Drink>> list = new ManageList<ManageList<Drink>>();
+        private void GetListDrinkPerType()
+        {
+            //Tạo list temp để làm trung gian lưu trữ
+            ManageList<Drink> temp = new ManageList<Drink>();
+            /* Duyệt từng listChoosenType một (string) , duyệt listDrink tìm loại Drink có Type giống
+             với listChoosenType[i] rồi kết hợp chúng thành List temp, thêm temp vào list ta được list[i]
+             */
+            for (int i = 0; i < listChooseType.Count; i++)
+            {
+                temp.Clear();
+                for (int j = 0; j < listDrink.Count; j++)
+                {
+                    if (listDrink[j].Type == listChooseType[i])
+                        temp.Add(listDrink[j]);
+                }
+                list.Add(temp);
+            }
+        }
+        /*-------------------------------------------------------------------------------------*/
+        private float Money;
+        private void GetMoney()
+        {
+            float BeforeMoney = float.Parse(txtMoney.Text);
+            float Discount = float.Parse(txtDiscount.Text);
+            Money = BeforeMoney * (1 + Discount / 100);
+        }
+        /*-------------------------------------------------------------------------------------*/
+
+        private ManageList<Drink> result = new ManageList<Drink>();
+        private float SUM = 0;
+        private void Try(int i)
+        {
+            for (int j = 0; j < list[i].Count; j++)
+            {
+                if (list[i][j].Price <= Money && list[i][j].Price + SUM <= Money)
+                {
+                    SUM += list[i][j].Price;
+                    result.Add(list[i][j]);
+                }
+                //Neu khong tim thay thi dung ham de quy do de quay lui
+                if (result[i] == null)
+                {
+                    result.RemoveAt(i);
+                    return;
+                }
+                else if (i == list.Count - 1) return;
+                else
+                {
+                    Try(i + 1);
+                }
+            }
+        }
+
+        private void getCombo()
+        {
+            Try(0);
+            for (int i = 0; i < result.Count; i++)
+            {
+                lsvResult.Items.Add(result[i].Name);
+            }
+        }
+        /*-------------------------------------------------------------------------------------*/
+              private void btnCombo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                GetMoney();
+                GetListDrinkPerType();
+                getCombo();
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message);
+                //MessageBox.Show("Error ! Please Check Input Again ! ");
+            }
+        }
     }
 }
+
+/*
+ Thực tế thì số tiền thường lớn  + số loại đồ uống ko quá nhiều + thuật toán chỉ lấy giá trị 
+tốt nhất nên dù quay lui ra rất nhiều kết quả nhưng thời gian chạy vẫn sẽ được tối ưu
+ */
